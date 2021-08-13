000aa8aba8cbdb12c71c18f6ea37402608dafb80
Brian W <brianwalters952@gmail.com>
2021-08-12 20:26:00 -0500

    Updates

---
+++ static/js/main.979e0707.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbucket=this.webpackJsonpbucket||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),o=c(3),s=c.n(o),i=c(0);var d=function(){return Object(i.jsx)("div",{children:"Hello World!!!"})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.979e0707.chunk.js.map
